8031b278b16886ca6ed9a297a8739be02b0d2d71
Gbunny <changwook987@naver.com>
2021-10-23 01:05:58 +0900

    날먹333

---
+++ src/kotlinFile/11729하노이_탑_이동_순서.kt
@@ -0,0 +1,30 @@
+package kotlinFile
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import kotlin.math.pow
+
+private val r = BufferedReader(InputStreamReader(System.`in`))
+private val w = BufferedWriter(OutputStreamWriter(System.out))
+
+fun move(size: Int, from: Int, to: Int) {
+    val tmp = 6.minus(from.plus(to))
+
+    if (size == 1) {
+        w.write("$from $to\n")
+        return
+    }
+    move(size.minus(1), from, tmp)
+    move(1, from, to)
+    move(size.minus(1), tmp, to)
+}
+
+fun main() {
+
+    val n = r.readLine().toInt()
+    w.write(2.0.pow(n.toDouble()).toInt().minus(1).toString()+"\n")
+    move(n, 1, 3)
+    w.flush()
+}
