c9a206bfd4531ee1af0057ccb984ce20b8966026
Gbunny <changwook987@naver.com>
2021-10-31 18:17:55 +0900

    날먹은 계속된다

---
+++ src/kotlinFile/10699오늘_날짜.kt
@@ -0,0 +1,8 @@
+package kotlinFile
+
+import java.util.*
+
+fun main() {
+    val calendar = GregorianCalendar()
+    println("%04d-%02d-%02d".format(calendar.weekYear, calendar[2].plus(1), calendar[5]))
+}
