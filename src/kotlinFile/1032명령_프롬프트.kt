f045119b98c82a9c7d7c0d50e4c8d8d6fb9540ff
Gbunny <changwook987@naver.com>
2021-11-03 16:15:41 +0900

    능지 떡락

---
+++ src/kotlinFile/1032명령_프롬프트.kt
@@ -0,0 +1,22 @@
+package kotlinFile
+
+fun main() {
+    val n = readLine()!!.toInt()
+
+    val list = List(n) { readLine()!! }
+    var str = ""
+    for (i in 0 until list[0].length) {
+        var miss = 0
+        var c = ' '
+        for (j in 0 until n) {
+            val char = list[j][i]
+            if (c != char) {
+                miss++
+                c = char
+            }
+        }
+
+        str += if (miss == 1) c else '?'
+    }
+    println(str)
+}
