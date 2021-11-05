00e61b932379b2cb231d8819236f9e8a7630d885
Gbunny <changwook987@naver.com>
2021-11-05 19:01:13 +0900

    ㅎ

---
+++ src/kotlinFile/2530인공지능_시계.kt
@@ -0,0 +1,19 @@
+package kotlinFile
+
+fun main() {
+    var h: Int
+    var m: Int
+    var s: Int
+    readLine()!!.split(' ').apply {
+        h = get(0).toInt()
+        m = get(1).toInt()
+        s = get(2).toInt()
+    }
+    s += readLine()!!.toInt()
+    m += s / 60
+    s %= 60
+    h += m / 60
+    m %= 60
+    h %= 24
+    println("$h $m $s")
+}
