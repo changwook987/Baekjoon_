45f3f79277499f92d9d37fb29e68262160e8602e
Gbunny <changwook987@naver.com>
2021-11-20 17:11:05 +0900

    뇌절...번개 잘랐다고..

---
+++ src/kotlinFile/6588골드바흐의_추측.kt
@@ -0,0 +1,30 @@
+package kotlinFile
+
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
+
+private fun Int.isPrime(): Boolean {
+    if (this < 2) return false
+    var i = 2
+    while (i * i <= this) {
+        if (this % i == 0) return false
+        i++
+    }
+    return true
+}
+
+fun main() {
+    val w = BufferedWriter(OutputStreamWriter(System.out))
+    while (true) {
+        val n = readLine()!!.toInt()
+        if (n == 0) break
+
+        for (i in (2..n.div(2))) {
+            if (i.isPrime() && n.minus(i).isPrime()) {
+                w.write("$n = $i + ${n.minus(i)}\n")
+                break
+            }
+        }
+    }
+    w.flush()
+}
