fcce26d1f559248d8dc4b7de3d1061d4773c6768
Gbunny <changwook987@naver.com>
2021-12-24 12:12:29 +0900

    zzZ

---
+++ src/NONBAEKJOON/javaFile/problem2819/Solution.java
@@ -0,0 +1,53 @@
+package NONBAEKJOON.javaFile.problem2819;
+
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class Solution {
+    private static void solution(Set<String> set, StringBuilder sb, int n, int[][] map, int x, int y) {
+        if (n == 7) {
+            set.add(sb.toString());
+        } else {
+            sb.append(map[x][y]);
+
+            solution(set, sb, n + 1, map, (x + 1) % 4, y);
+            solution(set, sb, n + 1, map, (x + 3) % 4, y);
+            solution(set, sb, n + 1, map, x, (y + 1) % 4);
+            solution(set, sb, n + 1, map, x, (y + 3) % 4);
+
+            sb.deleteCharAt(n);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            SortedSet<String> set = new TreeSet<>();
+
+            int[][] arr = new int[4][4];
+
+            for (int j = 0; j < 4; j++) {
+                for (int k = 0; k < 4; k++) {
+                    arr[j][k] = sc.nextInt();
+                }
+            }
+
+            for (int j = 0; j < 4; j++) {
+                for (int k = 0; k < 4; k++) {
+                    solution(set, new StringBuilder(), 0, arr, j, k);
+                }
+            }
+
+            for (String a : set) {
+                System.out.println(a);
+            }
+            System.out.printf("#%d %d\n", i + 1, set.size());
+        }
+    }
+}
---
+++ src/kotlinFile/2164카드2.kt
@@ -0,0 +1,20 @@
+package kotlinFile
+
+import java.util.*
+
+fun main() {
+    val n = readLine()!!.toInt()
+
+    val queue: Queue<Int> = LinkedList()
+
+    queue.addAll(List(n) { it + 1 })
+
+    while (queue.size != 1) {
+        with(queue) {
+            poll()
+            add(poll())
+        }
+    }
+
+    println(queue.peek())
+}
