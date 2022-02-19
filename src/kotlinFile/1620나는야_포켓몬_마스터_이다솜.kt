906539c24da9a03f70934f7479829e7b284a2922
changwook987 <changwook987@naver.com>
2022-02-19 20:44:28 +0900

    커밋 귀찮ㅇ

--- src/kotlinFile/1620나는야_포켓몬_마스터_이다솜.kt
+++ src/kotlinFile/1620나는야_포켓몬_마스터_이다솜.kt
@@ -8,19 +8,28 @@ import java.io.OutputStreamWriter
 fun main() {
     val r = BufferedReader(InputStreamReader(System.`in`))
     val w = BufferedWriter(OutputStreamWriter(System.out))
-    r.readLine().split(' ').apply {
-        val pokemonList = emptyMap<String, Int>().toMutableMap()
-        repeat(get(0).toInt()) {
-            pokemonList[r.readLine()] = it.plus(1)
+
+    val commands = r.readLine().split(' ')
+    val n = commands[0].toInt()
+    val m = commands[1].toInt()
+
+    val hashMap = HashMap<String, Int>()
+    val arr = ArrayList<String>()
+
+    repeat(n) {
+        with(r.readLine()) {
+            hashMap += this to it + 1
+            arr += this
         }
-        repeat(get(1).toInt()) {
-            val s = r.readLine()
-            s.toIntOrNull().apply {
-                if (this != null)
-                    w.write("${pokemonList.toList()[it]}\n")
-                else
-                    w.write("${pokemonList[s]}\n")
-            }
+    }
+
+    repeat(m) {
+        val input = r.readLine().run { toIntOrNull() ?: this }
+
+        if (input is Int) {
+            w.appendLine(arr[input - 1])
+        } else if (input is String) {
+            w.appendLine(hashMap[input].toString())
         }
     }
     w.flush()
---
+++ src/kotlinFile/1927최소_힙.kt
@@ -0,0 +1,18 @@
+package kotlinFile
+
+import java.util.*
+
+fun main() {
+    val queue = PriorityQueue<Int>()
+    val n = readLine()!!.toInt()
+
+    repeat(n) {
+        val input = readLine()!!.toInt()
+
+        if (input == 0) {
+            println(queue.poll() ?: 0)
+        } else {
+            queue += input
+        }
+    }
+}
