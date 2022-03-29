0c02c38072fad53586c18f2de546278e4f4a35a6
changwook987 <changwook987@naver.com>
2022-03-29 14:46:32 +0900

    baekjoon 1193

---
+++ src/kotlinFile/1193분수찾기.kt
@@ -0,0 +1,13 @@
+package kotlinFile
+
+fun main() {
+    val n = readln().toInt()
+    var i = 1
+    var N = n
+    while (N > i) {
+        N -= i
+        i++
+    }
+    if (i % 2 == 0) println("$N/${i - N + 1}")
+    else println("${i - N + 1}/$N")
+}
