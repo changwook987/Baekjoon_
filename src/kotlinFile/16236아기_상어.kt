bb937b2ea6d814eb7d53c63e4b1f2699bed81d0e
changwook987 <changwook987@naver.com>
2022-04-08 23:08:15 +0900

    baekjoon 16236 🦈

---
+++ src/kotlinFile/16236아기_상어.kt
@@ -0,0 +1,78 @@
+package kotlinFile
+
+import java.util.*
+
+fun main() {
+    data class Pos(val x: Int, val y: Int)
+
+    val queue: Queue<Pair<Pos, Int>> = LinkedList()
+
+    val n = readln().toInt()
+    val field = List(n) { y ->
+        with(StringTokenizer(readln())) {
+            IntArray(n) { x ->
+                nextToken().toInt().run {
+                    if (this == 9) {
+                        queue += Pos(x, y) to 0
+                        0
+                    } else this
+                }
+            }
+        }
+    }
+
+    var sharkSize = 2
+    var sharkEat = 0
+
+    var time = 0
+
+    while (true) {
+        val visited = ArrayList<Pos>()
+        val next = ArrayList<Pos>()
+
+        var leastDepth = Int.MAX_VALUE
+
+        while (queue.isNotEmpty()) {
+            val (pos, depth) = queue.poll()
+            val (x, y) = pos
+
+            if (x !in 0 until n || y !in 0 until n) continue
+            if (pos in visited) continue
+            if (field[y][x] !in 0..sharkSize) continue
+
+            visited += pos
+
+            if (field[y][x] in 1 until sharkSize) {
+                if (leastDepth >= depth) {
+                    next += pos
+                    leastDepth = depth
+                }
+            } else {
+                queue += Pos(x + 1, y) to depth + 1
+                queue += Pos(x - 1, y) to depth + 1
+                queue += Pos(x, y + 1) to depth + 1
+                queue += Pos(x, y - 1) to depth + 1
+            }
+        }
+
+        if (next.isEmpty()) {
+            break
+        } else {
+            if (++sharkEat >= sharkSize) {
+                sharkEat = 0
+                sharkSize++
+            }
+
+            time += leastDepth
+
+            next.sortedWith(compareBy({ it.y }, { it.x })).first().let {
+                queue += it to 0
+
+                val (x, y) = it
+                field[y][x] = 0
+            }
+        }
+    }
+
+    println(time)
+}
