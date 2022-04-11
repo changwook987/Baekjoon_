f33c262613bc5a7e9692042679ce7628751f5556
changwook987 <changwook987@naver.com>
2022-04-11 21:01:18 +0900

    baekjoon 1541

--- src/kotlinFile/1541잃어버린_괄호.kt
+++ src/kotlinFile/1541잃어버린_괄호.kt
@@ -1,43 +1,12 @@
 package kotlinFile
 
-import java.util.*
-
 fun main() {
-    val r = System.`in`.bufferedReader()
-    val w = System.out.bufferedWriter()
-    val n = r.readLine().toInt()
-    repeat(n) {
-        val cmd = r.readLine()
-        var left = true
+    val arr = readln().split("-").map { s -> s.split("+").sumOf { it.toInt() } }
+    var tot = arr.first()
 
-        r.readLine()
-
-        val deque: Deque<Int> = LinkedList(
-            with(StringTokenizer(r.readLine(), "[,]")) {
-                List(countTokens()) {
-                    nextToken().toInt()
-                }
-            }
-        )
-        try {
-            for (c in cmd) {
-                if (c == 'R') {
-                    left = !left
-                } else {
-                    if (left) {
-                        deque.pollFirst() ?: throw Exception()
-                    } else {
-                        deque.pollLast() ?: throw Exception()
-                    }
-                }
-            }
-            w.appendLine(
-                if (left) '[' + deque.joinToString(",") + ']'
-                else '[' + deque.reversed().joinToString(",") + ']'
-            )
-        } catch (e: Exception) {
-            w.appendLine("error")
-        }
+    for (i in 1 until arr.size) {
+        tot -= arr[i]
     }
-    w.flush()
+
+    println(tot)
 }
