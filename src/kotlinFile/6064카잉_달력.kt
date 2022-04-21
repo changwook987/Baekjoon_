10d3dae4c3fa79ffe0f0f5190dfb641a1bf25aef
changwook987 <changwook987@naver.com>
2022-04-21 18:23:10 +0900

    baekjoon 6064

---
+++ src/kotlinFile/6064카잉_달력.kt
@@ -0,0 +1,37 @@
+package kotlinFile
+
+fun main() {
+    fun gcd(a: Long, b: Long): Long {
+        return if (a % b == 0L) b
+        else gcd(b, a % b)
+    }
+
+    fun lcm(a: Long, b: Long): Long {
+        return a * b / gcd(a, b)
+    }
+
+    val list = System.`in`.bufferedReader().use { r ->
+        List(r.readLine().toInt()) {
+            r.readLine().split(" ").map { it.toLong() }
+        }
+    }
+
+
+    System.out.bufferedWriter().use {
+        for ((m, n, x, y) in list) {
+            var i = x
+            val limit = lcm(m, n)
+            while (true) {
+                if (i > limit || (i - 1) % n == y - 1) {
+                    break
+                }
+                i += m
+            }
+            it.appendLine(
+                if (i > limit) "-1"
+                else "$i"
+            )
+        }
+        it.flush()
+    }
+}
