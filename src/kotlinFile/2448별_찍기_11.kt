4a4c58074b08264085b91198ef0b093eb375c190
changwook987 <changwook987@naver.com>
2022-04-25 10:45:11 +0900

    baekjoon 2448

---
+++ src/kotlinFile/2448별_찍기_11.kt
@@ -0,0 +1,32 @@
+package kotlinFile
+
+fun main() {
+    val n = readln().toInt()
+    val star = List(n) { BooleanArray(n * 2) }
+
+    fun write(size: Int = n, x: Int = 0, y: Int = 0) {
+        if (size == 3) {
+            for ((i, value) in listOf("  *  ", " * * ", "*****").withIndex()) {
+                for ((j, c) in value.withIndex()) {
+                    star[y + i][x + j] = c == '*'
+                }
+            }
+        } else {
+            write(size / 2, x + size / 2, y)
+            write(size / 2, x, y + size / 2)
+            write(size / 2, x + size, y + size / 2)
+        }
+    }
+
+    write()
+
+    System.out.bufferedWriter().use {
+        for (i in star) {
+            for (j in i) {
+                it.append(if (j) '*' else ' ')
+            }
+            it.append('\n')
+        }
+        it.flush()
+    }
+}
