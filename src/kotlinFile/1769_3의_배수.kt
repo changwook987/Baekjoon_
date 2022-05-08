de6aeea5b278476355ad3ac1596c1dbafaec46b3
changwook987 <changwook987@naver.com>
2022-05-09 08:18:42 +0900

    baekjoon 1769

---
+++ src/kotlinFile/1769_3의_배수.kt
@@ -0,0 +1,17 @@
+package kotlinFile
+
+fun main() {
+    var i = readln()
+    var cnt = 0
+
+    while (i.length > 1) {
+        i = i.map { it.digitToInt() }.sum().toString()
+        cnt++
+    }
+
+    println(cnt)
+
+    if (i.toInt() % 3 == 0) println("YES")
+    else println("NO")
+}
+
