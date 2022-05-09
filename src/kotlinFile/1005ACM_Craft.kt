77402f0ec4817dcf029e2b819e9cb19ebd4789c0
changwook987 <changwook987@naver.com>
2022-05-09 16:14:34 +0900

    baekjoon 1005

---
+++ src/kotlinFile/1005ACM_Craft.kt
@@ -0,0 +1,43 @@
+package kotlinFile
+
+import java.util.*
+
+fun main() {
+    val r = System.`in`.bufferedReader()
+    val w = System.out.bufferedWriter()
+
+    fun r() = r.readLine().split(" ").map { it.toInt() }
+
+    val t = r.readLine().toInt()
+
+    repeat(t) {
+        val (vertex, edge) = r()
+        val time = listOf(0) + r()
+        val graph = List(vertex + 1) { ArrayList<Int>() } // parent graph
+
+        repeat(edge) {
+            val (s, e) = r()
+            graph[e] += s
+        }
+
+        val map = TreeMap<Int, Int>()
+
+        fun dfs(cur: Int): Int {
+            return map.getOrPut(cur) {
+                if (graph.isEmpty()) time[cur]
+                else {
+                    var m = 0
+                    for (p in graph[cur]) {
+                        m = maxOf(m, dfs(p))
+                    }
+                    m + time[cur]
+                }
+            }
+        }
+
+        val e = r.readLine().toInt()
+        w.appendLine("${dfs(e)}")
+    }
+
+    w.flush()
+}
